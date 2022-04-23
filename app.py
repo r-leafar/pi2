4ac3beb9a90466167e24ddbb9df03a2d143ff7c6
Rafael Saboia <r-leafar@hotmail.com>
2022-04-23 12:02:49 -0300

    Atualizado

--- app.py
+++ app.py
@@ -74,20 +74,25 @@ class ClienteResource(Resource):
         return response
     def delete(self,idcliente):
         cli = Cliente.query.filter_by(idcliente=idcliente).first()
-        cli.delete()
-        msg = "O cliente {} foi excluido com sucesso".format(cli.nome)
+        try:
+            cli.delete()
+            msg = "O cliente {} foi excluido com sucesso".format(cli.nome)
+            status = "sucesso"
+        except:
+            msg = "Não foi possível excluir {}".format(cli.nome)
+            status = "erro"
         return {
-            "status":"sucesso",
+            "status":status,
             "mensagem":msg
         }
 class ListarClienteResource(Resource):
     def get(self):
-        cli = Cliente.query.filter_by(idcliente=idcliente).first()
-        return 
+        cli = Cliente.query.all()
+        response = [ {"idcliente":i.idcliente,"nome":i.nome,"bairro":i.bairro,"logradouro":i.logradouro,"cidade":i.cidade,"numero":i.numero,"email":i.email,"criadoem":(i.criadoem- timedelta(hours=3)).strftime("%Y-%m-%d %H:%M:%S"),"alteradoem":(i.alteradoem - timedelta(hours=3)).strftime("%Y-%m-%d %H:%M:%S")} for i in cli]
+        return response
+        
     def post(self):
         dados = request.json
-        #cli = Cliente(nome=dados["nome"],bairro=dados["bairro"],
-        #logradouro=dados["logradouro"],cidade=dados["cidade"],numero=dados["numero"],email=dados["email"])
         cli=Cliente(**dados)
         cli.save()
         return {
@@ -109,10 +114,34 @@ class UsuarioResource(Resource):
             "alteradoem":(usuario.alteradoem - timedelta(hours=3)).strftime("%Y-%m-%d %H:%M:%S")
         }
     def put(self,idusuario):
+        usuario = Usuarios.query.filter_by(idusuario=idusuario).first()
         dados = request.json
-        usuario = Usuarios(**dados)
-        usuario.senha = generate_password_hash(dados["senha"])
+        if "login" in dados:
+            usuario.login = dados["login"]
+        if "senha" in dados:
+            usuario.senha = generate_password_hash(dados["senha"])
         usuario.save()
+        response ={
+                "idusuario":usuario.idusuario,
+                "login":usuario.login,
+                "senha":usuario.senha
+                }
+        return response
+        
+    def delete(self,idusuario):
+        usuario = Usuarios.query.filter_by(idusuario=idusuario).first()
+        try:
+            usuario.delete()
+            msg = "O usuario {} foi excluido com sucesso".format(usuario.login)
+            status = "sucesso"
+        except:
+            msg = "Não foi possível excluir {}".format(usuario.login)
+            status = "erro"
+        
+        return {
+            "status":status,
+            "mensagem":msg
+        }
         
 class ListarUsuarioResource(Resource):
     def get(self):
